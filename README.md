fb8750d3325bcebbbc3491523f27b684bc5ac300
Bobby Lei <bobl53@gmail.com>
2023-07-04 22:13:35 +1200

    Add visitor guest list

--- README.md
+++ README.md
@@ -4,5 +4,14 @@
 cargo run
 ```
 
+## Rust Notes
+### String conversion
+* To convert a &str to a String in Rust, you can use the to_string() method:
+* To convert a String to a &str, you can use the as_str() method
+  * Alternatively, you can use the & operator to get a reference to the String as a &str:
+
+## Self
+* Title case Self refers to the struct type itself.
+* Lowercase self refers to the instance of the structure.
 ## Upto
 Chapter 2
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
 use std::io::stdin;
 
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
-    your_name
-        .trim()
-        .to_lowercase()
+    your_name.trim().to_lowercase()
 }
 
 fn main() {
+    let visitor_list = [
+        Visitor::new("erica", "Hi wife, how was your day?"),
+        Visitor::new("zachy", "Hi Zachy, come and read some books"),
+        Visitor::new("conor", "Hi Conponshon, daddy want to kiss you"),
+        Visitor::new("rory", "Hi cutie pie"),
+    ];
+
     println!("Hello, what's your name?");
     let name = what_is_your_name();
-    println!("Hello, {}", name);
+
+    let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+
+    match known_visitor {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("Sorry, you aren't on the list."),
+    }
 }
