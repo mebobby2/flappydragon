0d53b860bae1a517a6739146257b49f191ef4855
Bobby Lei <bobl53@gmail.com>
2023-07-06 21:32:36 +1200

    Add game engine, game state, and game modes

--- README.md
+++ README.md
@@ -10,6 +10,56 @@ cargo run
 * To convert a String to a &str, you can use the as_str() method
   * Alternatively, you can use the & operator to get a reference to the String as a &str:
 
+### Semicolons
+https://stackoverflow.com/questions/26665471/are-semicolons-optional-in-rust
+
+Almost everything in Rust is an expression. An expression is something that returns a value. If you put a semicolon you are suppressing the result of this expression, which in most cases is what you want.
+
+On the other hand, this means that if you end your function with an expression without a semicolon, the result of this last expression will be returned. The same can be applied for a block in a match statement.
+
+```
+let a = {
+    let inner = 2;
+    inner * inner
+};
+```
+
+Here the expression inner * inner does not end with a semicolon, so its value is not suppressed. Since it is the last expression in the block, its value will be returned and assigned to a. If you put a semicolon on this same line, the value of inner * inner won't be returned.
+
+### ? Operator
+https://stackoverflow.com/questions/42917566/what-is-this-question-mark-operator-about
+
+```
+fn halves_if_even(i: i32) -> Result<i32, Error> {
+    if i % 2 == 0 {
+        Ok(i / 2)
+    } else {
+        Err(/* something */)
+    }
+}
+
+fn do_the_thing(i: i32) -> Result<i32, Error> {
+    let i = match halves_if_even(i) {
+        Ok(i) => i,
+        Err(e) => return Err(e),
+    };
+
+    // use `i`
+}
+```
+
+Too verbose. Shortened using:
+```
+fn do_the_thing(i: i32) -> Result<i32, Error> {
+    let i = halves_if_even(i)?;
+
+    // use `i`
+```
+
+What ? does here is equivalent to the match statement above with an addition. In short:
+* It unpacks the Result if OK
+* It returns the error if not, calling From::from on the error value to potentially convert it to another type.
+
 ## Self
 * Title case Self refers to the struct type itself.
 * Lowercase self refers to the instance of the structure.
--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,74 @@
-use std::io::stdin;
+use bracket_lib::prelude::*;
 
-#[derive(Debug)]
-struct Visitor {
-    name: String,
-    action: VisitorAction,
-    age: i8,
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
+struct State {
+    mode: GameMode,
 }
 
-#[derive(Debug)]
-enum VisitorAction {
-    Accept,
-    AcceptWithNote { note: String },
-    Refuse,
-    Probation,
-}
-
-impl Visitor {
-    fn new(name: &str, action: VisitorAction, age: i8) -> Self {
-        Self {
-            name: name.to_lowercase(),
-            action,
-            age,
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu,
         }
     }
 
-    fn greet_visitor(&self) {
-        match &self.action {
-            VisitorAction::Accept => println!("Welcome to the treehouse, {}", self.name),
-            VisitorAction::AcceptWithNote { note } => {
-                println!("Welcome to the treehouse, {}", self.name);
-                println!("{}", note);
-                if self.age < 21 {
-                    println!("Do not serve alcohol to {}", self.name);
-                }
-            }
-            VisitorAction::Probation => println!("{} is now a probationary member", self.name),
-            VisitorAction::Refuse => println!("Do not allow {} in!", self.name),
-        }
+    fn play(&mut self, ctx: &mut BTerm) {
+        self.mode = GameMode::End;
     }
-}
 
-fn what_is_your_name() -> String {
-    let mut your_name = String::new();
-    stdin()
-        .read_line(&mut your_name)
-        .expect("Failed to read line");
-    your_name.trim().to_lowercase()
-}
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
 
-fn main() {
-    let mut visitor_list = vec![
-        Visitor::new("erica", VisitorAction::Accept, 45),
-        Visitor::new(
-            "zachy",
-            VisitorAction::AcceptWithNote {
-                note: String::from("Get your own milk!"),
-            },
-            5,
-        ),
-        Visitor::new("conor", VisitorAction::Refuse, 3),
-        Visitor::new("rory", VisitorAction::Accept, 0),
-        Visitor::new("henry", VisitorAction::Probation, 10),
-    ];
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
 
-    loop {
-        println!("Hello, what's your name? (Leave empty and press Enter to quit)");
-        let name = what_is_your_name();
-
-        let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
-
-        match known_visitor {
-            Some(visitor) => visitor.greet_visitor(),
-            None => {
-                if name.is_empty() {
-                    break;
-                } else {
-                    println!("{} is not on the visitor list.", name);
-                    visitor_list.push(Visitor::new(&name, VisitorAction::Probation, 0));
-                }
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
             }
         }
     }
 
-    println!("The final list of visitors:");
-    println!("{:#?}", visitor_list);
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::End => self.dead(ctx),
+            GameMode::Playing => self.play(ctx),
+        }
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
+
+    main_loop(context, State::new())
 }
